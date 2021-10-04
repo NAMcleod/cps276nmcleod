3988c67278a2c747675cca4ab46a06d5d5feb3c5
Nick McLeod <nickmcleod@Nicks-MacBook-Air.local>
2021-10-04 17:15:35 -0400

    final? update

--- assignments/assignment2/project2c.php
+++ assignments/assignment2/project2c.php
@@ -1,24 +1,31 @@
 <?php
+/*
+This script creates a 15x5 table, each cell containing the row# and cell# by default. Editing of the table size
+can be done by editing the $rows/$cols variable, editing the default value in the function declaration, or by 
+inserting raw integers into the function call.
+*/
 
-//$rows = 15; //number of cells height
-//$cols = 5;  //number of cells across
 
-function makeTable($rows = 15, $cols = 5)  
+$rows = 15;  // variables to be passed to function in main.
+$cols = 5;  //  """""""""""""""""""""""""""""""""""""""""""
+
+function makeTable($r = 15, $c = 5) //r -> rows, c -> columns.  
 {
     echo "<table border=1>";
-        for ($i = 1; $i <= $rows; $i++)
+        for ($i = 1; $i <= $r; $i++)
         {
             echo "<tr>";
-            for ($j = 1; $j <= $cols; $j++)
+            for ($j = 1; $j <= $c; $j++)
             {
                 echo "<td>Row $i Cell $j</td>";
-            }//end inner loop (for j)   
+            }//end inner loop (for j)
+
             echo "</tr>";
         }//end outer loop (for i)
+        
     echo "</table>";
     return 0;
-}
-
+}//end function makeTable()
 ?>
 
 
@@ -30,9 +37,9 @@ function makeTable($rows = 15, $cols = 5)
     <meta http-equiv="X-UA-Compatible" content ="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>Project 2c</title>
+    <title>Project 2c: Table</title>
 <main>
 <?php
-    makeTable(); //function call creates table
+    makeTable($rows, $cols);   //function call makes table
 ?> 
 </head>
